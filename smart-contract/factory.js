457db756642544f86843250d3556d999aecaedeb
Asim Ali <hi@asimali.net>
2022-12-03 01:18:22 +0500

    contract reset

--- smart-contract/factory.js
+++ smart-contract/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x78567F2D69e0140D18336A15289A7ACb43E82a6F"
+  "0x2468EE879afa9cFCA0623ac6af1dF324A136DccA"
 );
 
 export default instance;
